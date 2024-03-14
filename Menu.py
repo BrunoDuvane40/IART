5ae2fdfaabd2fddb33f095bfd54e7bd1f54caac0
DavidCastro21 <up202108763@up.pt>
2024-03-14 20:21:23 +0000

    initial algorithm

--- Menu.py
+++ Menu.py
@@ -4,8 +4,16 @@ import graph as gr
 stream = pkg.generate_package_stream(10, 100)
 graph = gr.generate_graph(stream)
 
+result = gr.simmulated_annealing(graph, 100, 0.01, 1000)
+
+
 for vertex_id, vertex_data in graph.vertices.items():
     print("Vertex ID:", vertex_id)
     print("Package Type:", vertex_data['type'])
     print("Edges:", vertex_data['edges'])
     print()
+
+print("Result:")
+
+for vertex in result:
+    print(vertex)
--- graph.py
+++ graph.py
@@ -1,3 +1,9 @@
+import random 
+
+from random import sample, random
+
+from math import exp
+
 class Graph:
     def __init__(self):
         self.vertices = {}
@@ -29,5 +35,47 @@ def generate_graph(package_stream):
                 graph.add_edge(package1.id, package2.id, distance)
 
     return graph
+
+
+def simmulated_annealing(graph, initial_temperature, cooling_rate, iterations):
+    current_solution = list(graph.vertices.keys())
+    best_solution = current_solution
+    temperature = initial_temperature
+
+    for i in range(iterations):
+        new_solution = current_solution.copy()
+        new_solution = swap_vertices(new_solution)
+        current_cost = cost(graph, current_solution)
+        new_cost = cost(graph, new_solution)
+        if new_cost < current_cost:
+            current_solution = new_solution
+            if new_cost < cost(graph, best_solution):
+                best_solution = new_solution
+        else:
+            if acceptance_probability(current_cost, new_cost, temperature) > random():
+                current_solution = new_solution
+        temperature *= 1 - cooling_rate
+    return best_solution
+
+def swap_vertices(solution):
+    i, j = sample(range(len(solution)), 2)
+    solution[i], solution[j] = solution[j], solution[i]
+    return solution
+
+def cost(graph, solution):
+    cost = 0
+    for i in range(len(solution) - 1):
+        vertex1 = graph.vertices[solution[i]]
+        vertex2 = graph.vertices[solution[i + 1]]
+        for edge in vertex1['edges']:
+            if edge['to'] == solution[i + 1]:
+                cost += edge['weight']
+    return cost
+
+def acceptance_probability(current_cost, new_cost, temperature):
+    if new_cost < current_cost:
+        return 1.0
+    return exp((current_cost - new_cost) / temperature)
+
 
 
