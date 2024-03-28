c9e385d4672108789fab8b95f3fd90555bb611bc
bduvane <bduvane@junifeup.pt>
2024-03-28 17:07:39 +0000

    Hill Climbing Bruno, And Genetic other Bruno

--- Menu.py
+++ Menu.py
@@ -6,199 +6,55 @@ stream = pkg.generate_package_stream(10, 50)
 
 graph = gr.generate_graph(stream)
 
+initial_solution = list(graph.vertices.keys())
+
 print("Graph:")
 for vertex_id, vertex_data in graph.vertices.items():
     print("Vertex ID:", vertex_id)
     print("PAckage Data", vertex_data)
     print()
 
-result = gr.simmulated_annealing(graph, 100, 0.01, 1000)
+simmulated_annealing_result = gr.simmulated_annealing(graph, 100, 0.01, 1000)
 
-for i in result: 
-    if (i == 0):
-        result.remove(i)
+hill_climbing_result = gr.hill_Climbing(graph, initial_solution, 10)
 
-result.insert(0, 0)
+genetic_algorithm_result = gr.genetic_algorithm(graph, 100)
 
-def calculate_time(distance):
-    return (distance / 60) * 60 #Na verdade esta função é desnecessária, pois a distância por si só já é o tempo em minutos uma vez que a velocidade é 60 km/h e 1Km = 1 minuto
-    # Deixemos assim no entanto, para deixar o código mais claro
+print("Simmulated Annealing Result:", simmulated_annealing_result)
+print()
+print("Hill Climbing Result:", hill_climbing_result)
 
-def attach_current_distanceAndTime_traveled(result):
-    result_with_current_distance_traveled = []
+simmulated_annealing_result_with_timeDistance = gr.attach_current_distanceAndTime_traveled(graph,simmulated_annealing_result)
 
-    for i in range(len(result)):
-        if i == 0:
-            result_with_current_distance_traveled.append((result[i], 0))
-        else:
-            result_with_current_distance_traveled.append((result[i], gr.cost(graph, result[:i+1]), calculate_time(gr.cost(graph, result[:i+1]))))
+hill_climbing_result_with_timeDistance = gr.attach_current_distanceAndTime_traveled(graph, hill_climbing_result)
 
-    return result_with_current_distance_traveled
+genetic_algorithm_result_with_timeDistance = gr.attach_current_distanceAndTime_traveled(graph, genetic_algorithm_result)
 
-
-result_with_current_distanceAndTime_traveled = attach_current_distanceAndTime_traveled(result)
-
-
-
-def evaluation_function(graph, result, result_with_current_distanceAndTime_traveled):
-
-    total_delay_minutes = 0 
-
-    for i in range (1, len(result_with_current_distanceAndTime_traveled)):
-        if graph.vertices[result_with_current_distanceAndTime_traveled[i][0]]['type'] == 'urgent':
-            if(result_with_current_distanceAndTime_traveled[i][2] <= graph.vertices[result_with_current_distanceAndTime_traveled[i][0]]['delivery_time']):
-                total_delay_minutes += 0
-            else:
-                total_delay_minutes += result_with_current_distanceAndTime_traveled[i][2] - graph.vertices[result_with_current_distanceAndTime_traveled[i][0]]['delivery_time']
-
-
-    """
-    print("Total Delay Minutes:", total_delay_minutes)
-    print()
-    print()
-
-    print("Total Delay Cost:", total_delay_minutes * 0.3)
-    print()
-    print()
-    """
-
-    broken_packages = 0
-    broken_packages_additional_cost = 0
-
-    for i in range(1, len(result_with_current_distanceAndTime_traveled)):
-        if graph.vertices[result_with_current_distanceAndTime_traveled[i][0]]['type'] == 'fragile':
-            distance_covered = result_with_current_distanceAndTime_traveled[i][1]
-            chance_of_damage = graph.vertices[result_with_current_distanceAndTime_traveled[i][0]]['breaking_chance']
-            p_damage = 1 - ((1 - chance_of_damage) ** distance_covered)
-            """
-            print("Probability of Damage:", p_damage)
-            print()
-            print()
-            """
-            if (random.uniform(0, 1) < p_damage):
-                broken_packages += 1
-                broken_packages_additional_cost += graph.vertices[result_with_current_distanceAndTime_traveled[i][0]]['breaking_cost']
-
-    """
-
-    print("Broken Packages:", broken_packages)
-    print()
-    print()
-
-    print("Broken Packages Additional Cost:", broken_packages_additional_cost)
-    print()
-    print()
-
-    """
-
-    return gr.cost(graph, result) * 0.3 + (total_delay_minutes * 0.3) + broken_packages_additional_cost
-
-hill_Climbing_Initial_solution = list(graph.vertices.keys())
-
-def generate_neighbours(solution, num_neighbours=10):
-
-    print("Current Solution is" ,solution)
-    
-    neighbours = []
-    count = 0
-
-    for i in range(len(solution)):
-        if count >= num_neighbours:
-            break
-        for j in range(i+1, len(solution)):
-            neighbour = solution.copy()
-            neighbour[i], neighbour[j] = neighbour[j], neighbour[i]
-            neighbours.append(neighbour)
-            count += 1
-            if count >= num_neighbours:
-                break
-
-    return neighbours
-
-def Hill_Climbing(graph, initial_solution, iterations):
-
-    print("Starting Hill Climbing with initial solution:", initial_solution)
-    print("Initial Solution Cost:", evaluation_function(graph, initial_solution, attach_current_distanceAndTime_traveled(initial_solution)))
-    print()
-    print()
-
-    current_solution = initial_solution
-
-    best_solution = current_solution
-
-    best_solution_cost = evaluation_function(graph, best_solution, attach_current_distanceAndTime_traveled(best_solution))
-
-    for i in range(iterations):
-        neighbours = generate_neighbours(best_solution)
-        found_better_neighbour = False
-        
-        for neighbour in neighbours:
-
-            neighbour_cost = evaluation_function(graph, neighbour, attach_current_distanceAndTime_traveled(neighbour))
-
-            """
-            print("Neighbour Cost:", neighbour_cost)
-            print("Neighbour Solution:", neighbour)
-            print()
-            print()
-            print("Best Solution Cost:", best_solution_cost)
-            print("Best Solution:", best_solution)
-            print()
-            print()
-            """
-
-            if neighbour_cost < best_solution_cost:
-                best_solution = neighbour
-
-                """
-                print("Best Solution Updated")
-                print("New Best Solution:", best_solution)
-                print()
-                print()
-                """
-
-                best_solution_cost = neighbour_cost
-                found_better_neighbour = True
-                break
-
-        if not found_better_neighbour:
-            break
-                
-
-    print("Best Solution by hill climbing:", best_solution)
-
-    return best_solution
-
-result_of_hill = Hill_Climbing(graph, hill_Climbing_Initial_solution, 10)
-
-for i in result_of_hill: 
-    if (i == 0):
-        result_of_hill.remove(i)
-
-result_of_hill.insert(0, 0)
-
-result_with_current_distanceAndTime_traveled_hill = attach_current_distanceAndTime_traveled(result_of_hill)
-
-print("Result of Simmulated Annealing:")
-
-for vertex in result_with_current_distanceAndTime_traveled:
-    print(vertex)
-
+print("Simmulated Annealing Result with Time and Distance:", simmulated_annealing_result_with_timeDistance)
 print()
 print()
 
-print("Cost of Simmulated Annealing:", evaluation_function(graph, result, result_with_current_distanceAndTime_traveled))
-
+print("Hill Climbing Result with Time and Distance:", hill_climbing_result_with_timeDistance)
 print()
 print()
 
-
-print("Result of Hill Climbing:")
-for vertex in result_with_current_distanceAndTime_traveled_hill:
-    print(vertex)
-
-
-print("Cost of Hill Climbing:", evaluation_function(graph, result_of_hill, result_with_current_distanceAndTime_traveled_hill))
-
+print("Genetic algorithm result:", genetic_algorithm_result)
 print()
 print()
+
+print("Genetic algorithm result with time and distance:", genetic_algorithm_result_with_timeDistance)
+print()
+print()
+
+print("Genetic algorithm result cost:", gr.evaluation_function(graph, genetic_algorithm_result, genetic_algorithm_result_with_timeDistance)) 
+print()
+print()
+
+print("Simmulated Annealing Result Cost:", gr.evaluation_function(graph, simmulated_annealing_result, simmulated_annealing_result_with_timeDistance))
+print()
+print()
+
+print("Hill Climbing Result Cost:", gr.evaluation_function(graph, hill_climbing_result, hill_climbing_result_with_timeDistance))
+print()
+print()
+
--- graph.py
+++ graph.py
@@ -1,6 +1,5 @@
-import random 
 
-from random import sample, random
+import random
 
 from math import exp
 
@@ -66,13 +65,21 @@ def simmulated_annealing(graph, initial_temperature, cooling_rate, iterations):
             if new_cost < cost(graph, best_solution):
                 best_solution = new_solution
         else:
-            if acceptance_probability(current_cost, new_cost, temperature) > random():
+            if acceptance_probability(current_cost, new_cost, temperature) > random.random():
                 current_solution = new_solution
         temperature *= 1 - cooling_rate
+
+    
+    for j in best_solution:
+        if (j==0):
+            best_solution.remove(j)
+    
+    best_solution.insert(0,0)
+
     return best_solution
 
 def swap_vertices(solution):
-    i, j = sample(range(len(solution)), 2)
+    i, j = random.sample(range(len(solution)), 2)
     solution[i], solution[j] = solution[j], solution[i]
     return solution
 
@@ -90,3 +97,209 @@ def acceptance_probability(current_cost, new_cost, temperature):
     if new_cost < current_cost:
         return 1.0
     return exp((current_cost - new_cost) / temperature)
+
+
+def hill_Climbing(graph, initial_solution, iterations):
+
+    print("Starting Hill Climbing with initial solution:", initial_solution)
+    print("Initial Solution Cost:", evaluation_function(graph, initial_solution, attach_current_distanceAndTime_traveled(graph, initial_solution)))
+    print()
+    print()
+
+    current_solution = initial_solution
+
+    best_solution = current_solution
+
+    best_solution_cost = evaluation_function(graph, best_solution, attach_current_distanceAndTime_traveled(graph, best_solution))
+
+    for i in range(iterations):
+        neighbours = generate_neighbours(best_solution)
+        found_better_neighbour = False
+        
+        for neighbour in neighbours:
+
+            neighbour_cost = evaluation_function(graph, neighbour, attach_current_distanceAndTime_traveled(graph, neighbour))
+
+            """
+            print("Neighbour Cost:", neighbour_cost)
+            print("Neighbour Solution:", neighbour)
+            print()
+            print()
+            print("Best Solution Cost:", best_solution_cost)
+            print("Best Solution:", best_solution)
+            print()
+            print()
+            """
+
+            if neighbour_cost < best_solution_cost:
+                best_solution = neighbour
+
+                """
+                print("Best Solution Updated")
+                print("New Best Solution:", best_solution)
+                print()
+                print()
+                """
+
+                best_solution_cost = neighbour_cost
+                found_better_neighbour = True
+                break
+
+        if not found_better_neighbour:
+            break
+
+    for j in best_solution:
+        if (j==0):
+            best_solution.remove(j)
+    
+    best_solution.insert(0,0)
+                
+    print("Best Solution by hill climbing:", best_solution)
+
+    return best_solution
+
+def generate_neighbours(solution, num_neighbours=10):
+
+
+
+    #print("Current Solution is" ,solution)
+    
+    neighbours = []
+    count = 0
+
+    for i in range(len(solution)):
+        if count >= num_neighbours:
+            break
+        for j in range(i+1, len(solution)):
+            neighbour = solution.copy()
+            neighbour[i], neighbour[j] = neighbour[j], neighbour[i]
+            neighbours.append(neighbour)
+            count += 1
+            if count >= num_neighbours:
+                break
+
+    return neighbours
+
+def attach_current_distanceAndTime_traveled(graph,result):
+    result_with_current_distance_traveled = []
+
+    for i in range(len(result)):
+        if i == 0:
+            result_with_current_distance_traveled.append((result[i], 0))
+        else:
+            result_with_current_distance_traveled.append((result[i], cost(graph, result[:i+1]), calculate_time(cost(graph, result[:i+1]))))
+
+    return result_with_current_distance_traveled
+
+def calculate_time(distance):
+    return (distance / 60) * 60 #Na verdade esta função é desnecessária, pois a distância por si só já é o tempo em minutos uma vez que a velocidade é 60 km/h e 1Km = 1 minuto
+    # Deixemos assim no entanto, para deixar o código mais claro
+
+def evaluation_function(graph, result, result_with_current_distanceAndTime_traveled):
+
+    total_delay_minutes = 0 
+
+    for i in range (1, len(result_with_current_distanceAndTime_traveled)):
+        if graph.vertices[result_with_current_distanceAndTime_traveled[i][0]]['type'] == 'urgent':
+            if(result_with_current_distanceAndTime_traveled[i][2] <= graph.vertices[result_with_current_distanceAndTime_traveled[i][0]]['delivery_time']):
+                total_delay_minutes += 0
+            else:
+                total_delay_minutes += result_with_current_distanceAndTime_traveled[i][2] - graph.vertices[result_with_current_distanceAndTime_traveled[i][0]]['delivery_time']
+
+
+    """
+    print("Total Delay Minutes:", total_delay_minutes)
+    print()
+    print()
+
+    print("Total Delay Cost:", total_delay_minutes * 0.3)
+    print()
+    print()
+    """
+
+    broken_packages = 0
+    broken_packages_additional_cost = 0
+
+    for i in range(1, len(result_with_current_distanceAndTime_traveled)):
+        if graph.vertices[result_with_current_distanceAndTime_traveled[i][0]]['type'] == 'fragile':
+            distance_covered = result_with_current_distanceAndTime_traveled[i][1]
+            chance_of_damage = graph.vertices[result_with_current_distanceAndTime_traveled[i][0]]['breaking_chance']
+            p_damage = 1 - ((1 - chance_of_damage) ** distance_covered)
+            """
+            print("Probability of Damage:", p_damage)
+            print()
+            print()
+            """
+            if (random.uniform(0, 1) < p_damage):
+                broken_packages += 1
+                broken_packages_additional_cost += graph.vertices[result_with_current_distanceAndTime_traveled[i][0]]['breaking_cost']
+
+    """
+
+    print("Broken Packages:", broken_packages)
+    print()
+    print()
+
+    print("Broken Packages Additional Cost:", broken_packages_additional_cost)
+    print()
+    print()
+
+    """
+
+    return cost(graph, result) * 0.3 + (total_delay_minutes * 0.3) + broken_packages_additional_cost
+
+
+def genetic_algorithm(graph, iterations):
+
+    def generate_random_solution():
+        vertices = list(graph.vertices.keys())
+        random.shuffle(vertices)
+        return vertices
+    
+    def crossover(parent1, parent2):
+        child = parent1[:6]
+
+        remaining_vertices = set(parent2)-set(child)
+        for vertex in parent2:
+            if vertex in remaining_vertices:
+                child.append(vertex)
+                remaining_vertices.remove(vertex)
+
+        return child
+
+    population = [generate_random_solution() for _ in range(10)]
+
+    for _ in range(iterations):
+        parent1 = random.choice(population)
+
+        parent2 = random.choice(population)
+
+        while (parent1 == parent2):
+            parent2 = random.choice(population)
+
+        child = crossover(parent1, parent2)
+
+        child_cost = evaluation_function(graph, child, attach_current_distanceAndTime_traveled(graph, child))
+
+        worst_index = 0
+        for i, solution in enumerate(population):
+            curr_cost = evaluation_function(graph, solution, attach_current_distanceAndTime_traveled(graph, solution))
+            if curr_cost > evaluation_function(graph, population[worst_index], attach_current_distanceAndTime_traveled(graph, population[worst_index])):
+                worst_index = i
+
+        worst_cost = evaluation_function(graph, population[worst_index], attach_current_distanceAndTime_traveled(graph, population[worst_index]))
+        
+        if child_cost < worst_cost:
+            population[worst_index] = child
+
+    best_solution = min(population, key=lambda solution: evaluation_function(graph, solution, attach_current_distanceAndTime_traveled(graph, solution)))
+
+    for j in best_solution:
+        if (j==0):
+            best_solution.remove(j)
+    
+    best_solution.insert(0,0)
+
+    return best_solution
+
+
